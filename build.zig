5ce91cf93d5da6682c4445a6e349e1abbec05c7f
thrombe <yash.n.thombare620@gmail.com>
2025-03-09 21:48:58 +0530

    compile jolt in build-libs

--- build.zig
+++ build.zig
@@ -87,6 +87,173 @@ fn imgui_step(b: *std.Build, v: struct {
     };
 }
 
+fn jolt_step(b: *std.Build, v: struct {
+    jolt: *std.Build.Dependency,
+    target: ?std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+}) struct { lib: *std.Build.Step.Compile } {
+    const jolt = b.addSharedLibrary(.{
+        .name = "jolt",
+        .target = v.target,
+        .optimize = v.optimize,
+    });
+
+    jolt.addIncludePath(v.jolt.path("./"));
+    jolt.addCSourceFiles(.{
+        .root = v.jolt.path("./Jolt"),
+        .flags = &[_][]const u8{
+            "-std=c++17",
+            "-fno-exceptions",
+            "-fno-sanitize=undefined",
+
+            // "-DJPH_CROSS_PLATFORM_DETERMINISTIC",
+            // "-DJPH_DEBUG_RENDERER",
+            // "-DJPH_DOUBLE_PRECISION",
+            // "-DJPH_ENABLE_ASSERTS",
+        },
+        .files = &[_][]const u8{
+            "AABBTree/AABBTreeBuilder.cpp",
+            "Core/Color.cpp",
+            "Core/Factory.cpp",
+            "Core/IssueReporting.cpp",
+            "Core/JobSystemSingleThreaded.cpp",
+            "Core/JobSystemThreadPool.cpp",
+            "Core/JobSystemWithBarrier.cpp",
+            "Core/LinearCurve.cpp",
+            "Core/Memory.cpp",
+            "Core/Profiler.cpp",
+            "Core/RTTI.cpp",
+            "Core/Semaphore.cpp",
+            "Core/StringTools.cpp",
+            "Core/TickCounter.cpp",
+            "Geometry/ConvexHullBuilder.cpp",
+            "Geometry/ConvexHullBuilder2D.cpp",
+            "Geometry/Indexify.cpp",
+            "Geometry/OrientedBox.cpp",
+            "Math/Vec3.cpp",
+            "ObjectStream/ObjectStream.cpp",
+            "ObjectStream/ObjectStreamBinaryIn.cpp",
+            "ObjectStream/ObjectStreamBinaryOut.cpp",
+            "ObjectStream/ObjectStreamIn.cpp",
+            "ObjectStream/ObjectStreamOut.cpp",
+            "ObjectStream/ObjectStreamTextIn.cpp",
+            "ObjectStream/ObjectStreamTextOut.cpp",
+            "ObjectStream/SerializableObject.cpp",
+            "ObjectStream/TypeDeclarations.cpp",
+            "Physics/Body/Body.cpp",
+            "Physics/Body/BodyCreationSettings.cpp",
+            "Physics/Body/BodyInterface.cpp",
+            "Physics/Body/BodyManager.cpp",
+            "Physics/Body/MassProperties.cpp",
+            "Physics/Body/MotionProperties.cpp",
+            "Physics/Character/Character.cpp",
+            "Physics/Character/CharacterBase.cpp",
+            "Physics/Character/CharacterVirtual.cpp",
+            "Physics/Collision/BroadPhase/BroadPhase.cpp",
+            "Physics/Collision/BroadPhase/BroadPhaseBruteForce.cpp",
+            "Physics/Collision/BroadPhase/BroadPhaseQuadTree.cpp",
+            "Physics/Collision/BroadPhase/QuadTree.cpp",
+            "Physics/Collision/CastConvexVsTriangles.cpp",
+            "Physics/Collision/CastSphereVsTriangles.cpp",
+            "Physics/Collision/CollideConvexVsTriangles.cpp",
+            "Physics/Collision/CollideSphereVsTriangles.cpp",
+            "Physics/Collision/CollisionDispatch.cpp",
+            "Physics/Collision/CollisionGroup.cpp",
+            "Physics/Collision/EstimateCollisionResponse.cpp",
+            "Physics/Collision/GroupFilter.cpp",
+            "Physics/Collision/GroupFilterTable.cpp",
+            "Physics/Collision/ManifoldBetweenTwoFaces.cpp",
+            "Physics/Collision/NarrowPhaseQuery.cpp",
+            "Physics/Collision/NarrowPhaseStats.cpp",
+            "Physics/Collision/PhysicsMaterial.cpp",
+            "Physics/Collision/PhysicsMaterialSimple.cpp",
+            "Physics/Collision/Shape/BoxShape.cpp",
+            "Physics/Collision/Shape/CapsuleShape.cpp",
+            "Physics/Collision/Shape/CompoundShape.cpp",
+            "Physics/Collision/Shape/ConvexHullShape.cpp",
+            "Physics/Collision/Shape/ConvexShape.cpp",
+            "Physics/Collision/Shape/CylinderShape.cpp",
+            "Physics/Collision/Shape/DecoratedShape.cpp",
+            "Physics/Collision/Shape/EmptyShape.cpp",
+            "Physics/Collision/Shape/HeightFieldShape.cpp",
+            "Physics/Collision/Shape/MeshShape.cpp",
+            "Physics/Collision/Shape/MutableCompoundShape.cpp",
+            "Physics/Collision/Shape/OffsetCenterOfMassShape.cpp",
+            "Physics/Collision/Shape/PlaneShape.cpp",
+            "Physics/Collision/Shape/RotatedTranslatedShape.cpp",
+            "Physics/Collision/Shape/ScaledShape.cpp",
+            "Physics/Collision/Shape/Shape.cpp",
+            "Physics/Collision/Shape/SphereShape.cpp",
+            "Physics/Collision/Shape/StaticCompoundShape.cpp",
+            "Physics/Collision/Shape/TaperedCapsuleShape.cpp",
+            "Physics/Collision/Shape/TaperedCylinderShape.cpp",
+            "Physics/Collision/Shape/TriangleShape.cpp",
+            "Physics/Collision/TransformedShape.cpp",
+            "Physics/Constraints/ConeConstraint.cpp",
+            "Physics/Constraints/Constraint.cpp",
+            "Physics/Constraints/ConstraintManager.cpp",
+            "Physics/Constraints/ContactConstraintManager.cpp",
+            "Physics/Constraints/DistanceConstraint.cpp",
+            "Physics/Constraints/FixedConstraint.cpp",
+            "Physics/Constraints/GearConstraint.cpp",
+            "Physics/Constraints/HingeConstraint.cpp",
+            "Physics/Constraints/MotorSettings.cpp",
+            "Physics/Constraints/PathConstraint.cpp",
+            "Physics/Constraints/PathConstraintPath.cpp",
+            "Physics/Constraints/PathConstraintPathHermite.cpp",
+            "Physics/Constraints/PointConstraint.cpp",
+            "Physics/Constraints/PulleyConstraint.cpp",
+            "Physics/Constraints/RackAndPinionConstraint.cpp",
+            "Physics/Constraints/SixDOFConstraint.cpp",
+            "Physics/Constraints/SliderConstraint.cpp",
+            "Physics/Constraints/SpringSettings.cpp",
+            "Physics/Constraints/SwingTwistConstraint.cpp",
+            "Physics/Constraints/TwoBodyConstraint.cpp",
+            "Physics/DeterminismLog.cpp",
+            "Physics/IslandBuilder.cpp",
+            "Physics/LargeIslandSplitter.cpp",
+            "Physics/PhysicsScene.cpp",
+            "Physics/PhysicsSystem.cpp",
+            "Physics/PhysicsUpdateContext.cpp",
+            "Physics/Ragdoll/Ragdoll.cpp",
+            "Physics/SoftBody/SoftBodyCreationSettings.cpp",
+            "Physics/SoftBody/SoftBodyMotionProperties.cpp",
+            "Physics/SoftBody/SoftBodyShape.cpp",
+            "Physics/SoftBody/SoftBodySharedSettings.cpp",
+            "Physics/StateRecorderImpl.cpp",
+            "Physics/Vehicle/MotorcycleController.cpp",
+            "Physics/Vehicle/TrackedVehicleController.cpp",
+            "Physics/Vehicle/VehicleAntiRollBar.cpp",
+            "Physics/Vehicle/VehicleCollisionTester.cpp",
+            "Physics/Vehicle/VehicleConstraint.cpp",
+            "Physics/Vehicle/VehicleController.cpp",
+            "Physics/Vehicle/VehicleDifferential.cpp",
+            "Physics/Vehicle/VehicleEngine.cpp",
+            "Physics/Vehicle/VehicleTrack.cpp",
+            "Physics/Vehicle/VehicleTransmission.cpp",
+            "Physics/Vehicle/Wheel.cpp",
+            "Physics/Vehicle/WheeledVehicleController.cpp",
+            "RegisterTypes.cpp",
+            "Renderer/DebugRenderer.cpp",
+            "Renderer/DebugRendererPlayback.cpp",
+            "Renderer/DebugRendererRecorder.cpp",
+            "Renderer/DebugRendererSimple.cpp",
+            "Skeleton/SkeletalAnimation.cpp",
+            "Skeleton/Skeleton.cpp",
+            "Skeleton/SkeletonMapper.cpp",
+            "Skeleton/SkeletonPose.cpp",
+            "TriangleSplitter/TriangleSplitter.cpp",
+            "TriangleSplitter/TriangleSplitterBinning.cpp",
+            "TriangleSplitter/TriangleSplitterMean.cpp",
+        },
+    });
+
+    jolt.linkLibC();
+    jolt.linkLibCpp();
+
+    return .{ .lib = jolt };
+}
+
 const CompileMode = enum {
     exe,
     hotexe,
@@ -168,14 +335,20 @@ pub fn build(b: *std.Build) void {
 
     const vulkan_headers = b.dependency("vulkan_headers", .{});
     const imgui = b.dependency("imgui", .{});
+    const jolt = b.dependency("jolt", .{});
 
     const vulkan = vulkan_step(b, .{
         .vulkan_headers = vulkan_headers,
     });
-    const cimgui = imgui_step(b, .{
+    const libimgui = imgui_step(b, .{
         .dear = b.dependency("dear_bindings", .{}),
         .imgui = imgui,
         .vulkan_headers = vulkan_headers,
+        .target = target,
+        .optimize = optimize,
+    });
+    const libjolt = jolt_step(b, .{
+        .jolt = jolt,
         .target = target,
         .optimize = optimize,
     });
@@ -185,32 +358,34 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .mode = .exe,
         .vulkan_zig = vulkan.mod,
-        .cimgui = cimgui.lib,
         .imgui_dep = imgui,
-        .dcimgui_generated = cimgui.generated,
+        .cimgui = libimgui.lib,
+        .dcimgui_generated = libimgui.generated,
     });
     const hotlib = step(b, .{
         .target = target,
         .optimize = optimize,
         .mode = .hotlib,
         .vulkan_zig = vulkan.mod,
-        .cimgui = cimgui.lib,
         .imgui_dep = imgui,
-        .dcimgui_generated = cimgui.generated,
+        .cimgui = libimgui.lib,
+        .dcimgui_generated = libimgui.generated,
     });
     const hotexe = step(b, .{
         .target = target,
         .optimize = optimize,
         .mode = .hotexe,
         .vulkan_zig = vulkan.mod,
-        .cimgui = cimgui.lib,
         .imgui_dep = imgui,
-        .dcimgui_generated = cimgui.generated,
+        .cimgui = libimgui.lib,
+        .dcimgui_generated = libimgui.generated,
     });
 
     const build_libs_step = b.step("build-libs", "Build the libs required for the app");
-    build_libs_step.dependOn(&cimgui.lib.step);
-    build_libs_step.dependOn(&b.addInstallArtifact(cimgui.lib, .{}).step);
+    build_libs_step.dependOn(&libimgui.lib.step);
+    build_libs_step.dependOn(&b.addInstallArtifact(libimgui.lib, .{}).step);
+    build_libs_step.dependOn(&libjolt.lib.step);
+    build_libs_step.dependOn(&b.addInstallArtifact(libjolt.lib, .{}).step);
 
     const hot_build_step = b.step("build-hot", "Build the hot app");
     hot_build_step.dependOn(&b.addInstallArtifact(hotlib, .{}).step);
