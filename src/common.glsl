8d6c94df59125c95e1e468ebbe83786b53ece16f
thrombe <yash.n.thombare620@gmail.com>
2025-06-14 02:50:52 +0530

    add hsv <-> rgb to common.glsl

--- src/common.glsl
+++ src/common.glsl
@@ -69,6 +69,23 @@ int divide_roof(int a, int b) {
     return a / b + int(mod(a, b) > 0);
 }
 
+// - [hsv rgb conversion glsl shader · GitHub](https://gist.github.com/983/e170a24ae8eba2cd174f)
+vec3 rgb2hsv(vec3 c) {
+    vec4 k = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, k.wz), vec4(c.gb, k.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 k = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + k.xyz) * 6.0 - k.www);
+    return c.z * mix(k.xxx, clamp(p - k.xxx, 0.0, 1.0), c.y);
+}
+
 vec3 reinhard_tonemap(vec3 x) {
     return x / (x + vec3(1.0));
 }
